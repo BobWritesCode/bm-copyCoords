057bed42f5dc6301f3db10a2b4a3085804dde45c
BobWritesCode <warwick_hart@hotmail.com>
2024-03-20 12:20:26 +0000

    Fix clipboard copying, rm JQuery

--- html/script.js
+++ html/script.js
@@ -1,12 +1,12 @@
+let isDebug = false;
+
 document.onreadystatechange = () => {
   if (document.readyState === 'complete') {
     window.addEventListener('message', function (e) {
       switch (e.data.action) {
         case 'copy':
-          $('#copyThis').val(e.data.toCopy)
-          var copyText = $('#copyThis')
-          copyText.select();
-          document.execCommand("copy");
+          DebugLog('NUI message received: ^3copy');
+          CopyToClipboard(String(e.data.toCopy));
           break;
         default:
           break;
@@ -14,3 +14,27 @@ document.onreadystatechange = () => {
     });
   }
 };
+
+function CopyToClipboard(str) {
+  DebugLog('JS function: ^3CopyToClipboard()');
+  const el = document.createElement('textarea');
+  el.value = str;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+  DebugLog('Should have copied text', str);
+}
+
+function DebugLog(str = null, _str2 = null) {
+  if (!isDebug) {
+    return;
+  }
+  if (str && _str2) {
+    let str2 = String(_str2);
+    console.log('^4[Debug] ^2' + str + ' ^3' + str2 + '^7');
+  }
+  if (str && !_str2) {
+    console.log('^4[Debug] ^2' + str + '^7');
+  }
+}
